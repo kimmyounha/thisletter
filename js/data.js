309d06fe1ea5a913763cdc227692208cd3a87688
kimmyounha <w2224@e-mirim.hs.kr>
2023-12-21 23:13:05 +0900

    mailboxtree css 수정 및 data 추가

--- js/data.js
+++ js/data.js
@@ -1,1 +1,39 @@
-// 편지 받는 사람 이름, 이미지(장식), 편지 내용 json으로 작성
+let letters = [
+  {
+    "id": 1,
+    "receiver_name": "수신자의 이름1",
+    "sender_name": "송신자의 이름1",
+    "content": "편지 내용이 여기에 들어갑니다1."
+  },
+  {
+    "id": 2,
+    "receiver_name": "수신자의 이름2",
+    "sender_name": "송신자의 이름2",
+    "content": "편지 내용이 여기에 들어갑니다2."
+  },
+  {
+    "id": 3,
+    "receiver_name": "수신자의 이름3",
+    "sender_name": "송신자의 이름3",
+    "content": "편지 내용이 여기에 들어갑니다3."
+  },
+  {
+    "id": 4,
+    "receiver_name": "수신자의 이름1",
+    "sender_name": "송신자의 이름1",
+    "content": "편지 내용이 여기에 들어갑니다1."
+  },
+  {
+    "id": 5,
+    "receiver_name": "수신자의 이름2",
+    "sender_name": "송신자의 이름2",
+    "content": "편지 내용이 여기에 들어갑니다2."
+  },
+  {
+    "id": 6,
+    "receiver_name": "수신자의 이름3",
+    "sender_name": "송신자의 이름3",
+    "content": "편지 내용이 여기에 들어갑니다3."
+  }
+  
+]
