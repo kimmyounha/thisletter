e9645a41769301136c3f3456a5044552d3bd9d1c
kimmyounha <w2224@e-mirim.hs.kr>
2023-12-24 23:03:38 +0900

    password 변경

--- js/data-f.js
+++ js/data-f.js
@@ -2,58 +2,58 @@
 let letters = [
   {
     "id": 1,
-    "receiver_name": "수신자의 이름1",
-    "sender_name": "송신자의 이름1",
+    "receiver_name": "İlknur",
+    "sender_name": "윤하",
     "content": "편지 내용이 여기에 들어갑니다1.",
-    "password": "1234"
+    "password": "0404"
   },
   {
     "id": 2,
-    "receiver_name": "수신자의 이름2",
-    "sender_name": "송신자의 이름2",
+    "receiver_name": "최아인",
+    "sender_name": "응애윤하",
     "content": "편지 내용이 여기에 들어갑 sahl hldish flisdhf jsdhljfkhdsjfh sdfhlsdjhfl  sdfhlsdjfh sdflkhsldj fskdfhl sljfhlsjkhf slkfh sldjkfh qooijsdfio sdhflsdh f 니다2.",
-    "password": "1234"
+    "password": "0211"
   },
   {
     "id": 3,
-    "receiver_name": "수신자의 이름3",
-    "sender_name": "송신자의 이름3",
+    "receiver_name": "홍성아",
+    "sender_name": "박명수",
     "content": "편지 내용이 여기에 들어갑니다3.",
-    "password": "1234"
+    "password": "1219"
   },
   {
     "id": 4,
-    "receiver_name": "수신자의 이름3",
-    "sender_name": "송신자의 이름3",
+    "receiver_name": "헤미",
+    "sender_name": "윤하",
     "content": "편지 내용이 여기에 들어갑니다3.",
-    "password": "1234"
+    "password": "0822"
   },
   {
     "id": 5,
-    "receiver_name": "수신자의 이름3",
-    "sender_name": "송신자의 이름3",
+    "receiver_name": "이가현",
+    "sender_name": "너의 아기 유나",
     "content": "편지 내용이 여기에 들어갑니다3.",
-    "password": "1234"
+    "password": "0818"
   },
   {
     "id": 6,
-    "receiver_name": "수신자의 이름3",
+    "receiver_name": "김민채",
     "sender_name": "송신자의 이름3",
     "content": "편지 내용이 여기에 들어갑니다3.",
-    "password": "1234"
+    "password": "0709"
   },
   {
     "id": 7,
-    "receiver_name": "수신자의 이름3",
+    "receiver_name": "박도연",
     "sender_name": "송신자의 이름3",
     "content": "편지 내용이 여기에 들어갑니다3.",
-    "password": "1234"
+    "password": "1201"
   },
   {
     "id": 8,
-    "receiver_name": "수신자의 이름3",
+    "receiver_name": "윤민서",
     "sender_name": "송신자의 이름3",
     "content": "편지 내용이 여기에 들어갑니다3.",
-    "password": "1234"
+    "password": "1214"
   }
 ]
