9b98965029811ce8f10b8eaf934a97a81d579b2d
kimmyounha <w2224@e-mirim.hs.kr>
2023-12-24 20:55:32 +0900

    .

---
+++ js/data-t.js
@@ -0,0 +1,59 @@
+// 편지 받는 사람 이름, 이미지(장식), 편지 내용 json으로 작성
+let letters = [
+  {
+    "id": 1,
+    "receiver_name": "수신자의 이름1",
+    "sender_name": "송신자의 이름1",
+    "content": "편지 내용이 여기에 들어갑니다1.",
+    "password": "1234"
+  },
+  {
+    "id": 2,
+    "receiver_name": "수신자의 이름2",
+    "sender_name": "송신자의 이름2",
+    "content": "편지 내용이 여기에 들어갑 sahl hldish flisdhf jsdhljfkhdsjfh sdfhlsdjhfl  sdfhlsdjfh sdflkhsldj fskdfhl sljfhlsjkhf slkfh sldjkfh qooijsdfio sdhflsdh f 니다2.",
+    "password": "1234"
+  },
+  {
+    "id": 3,
+    "receiver_name": "수신자의 이름3",
+    "sender_name": "송신자의 이름3",
+    "content": "편지 내용이 여기에 들어갑니다3.",
+    "password": "1234"
+  },
+  {
+    "id": 4,
+    "receiver_name": "수신자의 이름3",
+    "sender_name": "송신자의 이름3",
+    "content": "편지 내용이 여기에 들어갑니다3.",
+    "password": "1234"
+  },
+  {
+    "id": 5,
+    "receiver_name": "수신자의 이름3",
+    "sender_name": "송신자의 이름3",
+    "content": "편지 내용이 여기에 들어갑니다3.",
+    "password": "1234"
+  },
+  {
+    "id": 6,
+    "receiver_name": "수신자의 이름3",
+    "sender_name": "송신자의 이름3",
+    "content": "편지 내용이 여기에 들어갑니다3.",
+    "password": "1234"
+  },
+  {
+    "id": 7,
+    "receiver_name": "수신자의 이름3",
+    "sender_name": "송신자의 이름3",
+    "content": "편지 내용이 여기에 들어갑니다3.",
+    "password": "1234"
+  },
+  {
+    "id": 8,
+    "receiver_name": "수신자의 이름3",
+    "sender_name": "송신자의 이름3",
+    "content": "편지 내용이 여기에 들어갑니다3.",
+    "password": "1234"
+  }
+]
